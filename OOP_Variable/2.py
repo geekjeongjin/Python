acb98ca91bb5670491a8c8a19d63e29fa91859d1
geekjeongjin <geekjeongjin@gmail.com>
2018-05-23 22:25:12 +0900

    OOP_Variable 폴더에 2.py, 3.py, 4.py 추가

---
+++ OOP_Variable/2.py
@@ -0,0 +1,18 @@
+class C(object):
+    def __init__(self, v):
+        self.value = v
+
+    def show(self):
+        print(self.value)
+
+    def getValue(self):
+        return self.value
+
+    def setValue(self, v):
+        self.value = v
+
+
+c1 = C(10)
+print(c1.value)
+c1.setValue(20)
+print(c1.getValue())
