75e943affb360465c00750f4902def3501251a39
geekjeongjin <geekjeongjin@gmail.com>
2018-06-12 06:26:01 +0900

    add

---
+++ Multiple_Inheritance/1.py
@@ -0,0 +1,27 @@
+class C1():
+    def c1_m(self):
+        print("c1_m")
+
+    def m(self):
+        print("C1_m")
+
+
+class C2():
+    def c2_m(self):
+        print("c2_m")
+
+    def m(self):
+        print("C2_m")
+
+
+# class C3(C1, C2):
+class C3(C2, C1):
+    # pass
+    def m(self):
+        print("C3_m")
+
+c = C3()
+c.c1_m()
+c.c2_m()
+c.m()
+print(C3.__mro__)
